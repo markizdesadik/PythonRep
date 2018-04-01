e2e4c04a06b874c21ad05c4ba67ba9f7cb746ca2
Labotskij Vasil <36326352+markizdesadik@users.noreply.github.com>
2018-04-01 19:35:42 +0300

    Add files via upload

--- HW4/DZ-4text.py
+++ HW4/DZ-4text.py
@@ -1,3 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Labotskiy Vasiliy
+
+"""Задание 1: Частота буквы.
+
+УСЛОВИЕ:
+Посчитать процентное соотношение букв в тексте. Заглавные и строчные приравниваются.
+Вывести словарь: ключ - буква, значение - процент (до десятых), в котором эта буква встречается в тексте.
+
+Текст:
+Proin eget tortor risus. Cras ultricies ligula sed magna dictum porta. Donec rutrum congue leo eget malesuada.
+
+Пример:
+"ABCda" >> {'a': 40.0, 'b': 20.0, 'c': 20.0, 'd': 20.0}
+"""
+
 def percentage(text, abw='qwertyuiopasdfghjklzxcvbnm'):
     List1 = [f for f in this_text.lower() if f in abw]
     List = {}
@@ -11,6 +28,7 @@ def percentage(text, abw='qwertyuiopasdfghjklzxcvbnm'):
     return List
 
 
-this_text = 'AsBCda'
+this_text = 'Proin eget tortor risus. Cras ultricies ligula sed magna dictum porta. ' \
+            'Donec rutrum congue leo eget malesuada.'
 
 print(percentage(this_text))
